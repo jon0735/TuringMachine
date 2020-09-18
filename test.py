49271fc251b92d67bcd8d995c438f0ea41fbbc09
jon0735 <jon0735@hotmail.com>
2020-09-18 07:44:03 +0200

    First

---
+++ test.py
@@ -0,0 +1,39 @@
+import turingmachine as tm
+import sys
+import time
+
+# def delLines():
+#     sys.stdout.write("\033[K")
+#     sys.stdout.write("\033[F")
+#     sys.stdout.write("\033[K")
+
+# tape = tm.Tape()
+
+# sys.stdout.write(str(tape))
+
+# tape.shift(-1)
+# tape.write("M")
+# tape.shift(-1)
+# tape.shift(-1)
+
+# time.sleep(1)
+
+# sys.stdout.write("\033[K")
+# sys.stdout.write("\033[F")
+# sys.stdout.write("\033[K")
+# # sys.stdout.write("STUFF")
+
+# time.sleep(1)
+
+
+# sys.stdout.write(str(tape))
+# time.sleep(1)
+
+# # tape.shift(1)
+# tape.shift(1)
+
+# print(tape.read())
+
+sys.stdout.write("ABCD")
+sys.stdout.write("\b\b")
+sys.stdout.write("ABCD")
---
+++ turingmachine.py
@@ -0,0 +1,77 @@
+
+
+# class State:
+#     pass
+
+
+class Tape:
+    def __init__(self, initSize=10):
+        self.head = 0
+        # sizes = initSize // 3
+        self.leftSize = initSize // 3
+        self.rightSize = initSize - self.leftSize
+        self.leftTape = [' '] * self.leftSize
+        self.rightTape = [' '] * self.rightSize
+
+    def __getitem__(self, key):  # override indexing in a get situation (var = tape[x])
+        if key < 0:
+            return self.leftTape[abs(key)-1]
+        else:
+            return self.rightTape[key]
+
+    def __setitem__(self, key, val):  # override indexing in a set situation (tape[x] = var)
+        if key < 0:
+            self.leftTape[abs(key)-1] = val
+        else:
+            self.rightTape[abs(key)] = val
+
+    def __str__(self):  #  String of tape shows a visual representation of the tape with header
+        leftIndexString = str(-self.leftSize) + ' ' 
+        rightIndexString = ' ' + str(self.rightSize - 1)
+        tapeString = leftIndexString + str(self.leftTape[::-1] + self.rightTape) + rightIndexString
+        headPrint = (" " * ((self.leftSize + self.head)*5 + 2 + len(leftIndexString)))
+        return tapeString + "\n" + headPrint + "^"
+
+    def read(self):
+        return self[self.head]
+
+    def write(self, char):
+        if len(char) != 1:
+            raise Exception("Can only write char (or strings of length 1). Given: " + char)
+        self[self.head] = char
+
+    def shift(self, direction):
+        if direction == 0:
+            return
+        elif direction == 1:
+            self.head += 1
+            if self.head >= self.rightSize:
+                self.rightTape = self.rightTape + ([' '] * self.rightSize)
+                self.rightSize = self.rightSize * 2
+        elif direction == -1:
+            self.head -= 1
+            if abs(self.head) > self.leftSize:
+                self.leftTape = self.leftTape + ([' '] * self.leftSize)
+                self.leftSize = self.leftSize * 2
+        else:
+            raise Exception("Can only shift 1, -1 or 0, not " + str(direction))
+    
+
+
+class TuringMachine:
+    def __init__(self):
+        self.states = []
+        self.alphabet = []  # Should be a set?
+        self.tapes = []  # tape 0 is input tape, and last is output tape
+        self.currentState = 0
+
+    def step(self):
+        tapeSymbols = [tape.read() for tape in self.tapes]
+        state = self.states[self.currentState]
+        for symbol in tapeSymbols:
+            state = state[symbol]
+        
+
+        # TODO
+        pass
+
